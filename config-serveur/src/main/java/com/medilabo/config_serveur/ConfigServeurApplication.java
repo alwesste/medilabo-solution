a6baa25b81660289fee87d60262e60963ecda09e
Leopold <leopolddag@gmail.com>
2026-04-03 17:15:28 +0200

    Mise em place micro-services de base

---
+++ config-serveur/src/main/java/com/medilabo/config_serveur/ConfigServeurApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.config_serveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServeurApplication.class, args);
+	}
+
+}
---
+++ config-serveur/src/test/java/com/medilabo/config_serveur/ConfigServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.config_serveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
