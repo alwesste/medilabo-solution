416ee4f0563a0eff5a68b348fadb0f76c4eef0e0
Leopold <leopolddag@gmail.com>
2026-05-23 23:19:13 +0200

    Deplacement de spring security dans la gateway

--- gateway/src/main/java/com/medilabo/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/medilabo/gateway/config/SecurityConfig.java
@@ -6,29 +6,67 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+/**
+ * Configuration de la securite de l'application, definit l'acces aux ressources de l'application ainsi
+ * que la strategie de gestion statless du serveur.
+ *
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     /**
-     * Configure les routes authorises, les sessions, logout et filtre personalise
-     * @param http
-     * @return la securityFilterChain
+     *
+     * Configure les routes autorisees, les sessions, logout et filtres personalise
+     * @return  la SecurityFilterChain, la chaine de filtre de spring-security
      * @throws Exception
      */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(
-                                SessionCreationPolicy.STATELESS)
+                .authorizeHttpRequests(r -> r
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/patients", true)
+                        .permitAll()
                 )
-                .securityMatcher("/**")
-                .authorizeHttpRequests(r -> r.anyRequest().permitAll());
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout").permitAll());
         return http.build();
     }
 
+    /**
+     * Definit les utilisateurs pouvant acceder à l'application
+     * @return le service de gestion des utilisateurs utilise par spring-secutity
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("leo")
+                .password(passwordEncoder().encode("leo123"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    /**
+     *
+     * @return un PasswordEncoder pour le hashage de mot de passe
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
